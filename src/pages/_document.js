f8e11782fb9988c3e1dee53ac1cfbf0e728d1c75
Petr Hoskovec <ihoskovecpetr@gmail.com>
2021-09-29 15:46:55 +0200

    Add SSR collect server styled

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,18 +2,41 @@ import Document, { Head, Main, NextScript, Html } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class CustomDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    function handleCollectStyles(App) {
-      return (props) => {
-        return sheet.collectStyles(<App {...props} />);
+    // function handleCollectStyles(App) {
+    //   return (props) => {
+    //     return sheet.collectStyles(<App {...props} />);
+    //   };
+    // }
+
+    // const page = ctx.renderPage((App) => handleCollectStyles(App));
+    // const styleTags = sheet.getStyleElement();
+    // return { ...page, styleTags };
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
+    } finally {
+      sheet.seal();
     }
-
-    const page = renderPage((App) => handleCollectStyles(App));
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
   }
 
   render() {
