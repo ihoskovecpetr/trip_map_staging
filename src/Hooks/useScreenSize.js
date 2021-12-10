f7524f46aaf971691a00eb7bb17a472c2fb6b526
Petr Hoskovec <ihoskovecpetr@gmail.com>
2021-12-10 10:27:48 +0100

    WIIP

--- src/Hooks/useScreenSize.js
+++ src/Hooks/useScreenSize.js
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from "react";
+const isClient = typeof window === "object";
 
 export function useScreenSize() {
-  const isClient = typeof window === "object";
+  const [windowSize, setWindowSize] = useState(getSize);
 
-  function getSize() {
+  function getSize(e) {
+    const viewport = e?.target;
+
+    console.log({ isClient });
+
     return {
       width: isClient ? window.innerWidth : undefined,
       height: isClient ? window.innerHeight : undefined,
+      visualViewportHeight: isClient && viewport ? viewport.height : undefined,
+      visualViewportWidth: isClient && viewport ? viewport.width : undefined,
     };
   }
-
-  const [windowSize, setWindowSize] = useState(getSize);
 
   useEffect(() => {
     if (!isClient) {
       return false;
     }
 
-    function handleResize() {
-      setWindowSize(getSize());
+    function handleResize(e) {
+      const newSizes = getSize(e);
+      setWindowSize(newSizes);
     }
 
     window.addEventListener("resize", handleResize);
     window.addEventListener("keydown", handleResize);
 
+    window.visualViewport.addEventListener("scroll", handleResize);
+    window.visualViewport.addEventListener("resize", handleResize);
+
     return () => {
       window.removeEventListener("resize", handleResize);
       window.removeEventListener("keydown", handleResize);
+
+      window.visualViewport.removeEventListener("scroll", handleResize);
+      window.visualViewport.removeEventListener("resize", handleResize);
     };
   }, []); // Empty array ensures that effect is only run on mount and unmount
 
