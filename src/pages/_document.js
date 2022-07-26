df6ed6aa6841d8577363a2c1d402c0a7b22038e2
ihoskovecpetr <petr@jadendigital.com.au>
2022-07-26 17:43:04 +1000

    Addinf full story

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,72 +1,68 @@
-import Document, { Head, Main, NextScript, Html } from "next/document";
-import { ServerStyleSheet } from "styled-components";
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from "@material-ui/core/styles";
+import Document, { Head, Main, NextScript, Html } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
 
 export default class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const styledComponentsSheet = new ServerStyleSheet();
-    const materialUiSheets = new MaterialUiServerStyleSheets();
+    static async getInitialProps(ctx) {
+        const styledComponentsSheet = new ServerStyleSheet()
+        const materialUiSheets = new MaterialUiServerStyleSheets()
 
-    const originalRenderPage = ctx.renderPage;
+        const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            styledComponentsSheet.collectStyles(
-              materialUiSheets.collect(<App {...props} />)
-            ),
-        });
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props =>
+                        styledComponentsSheet.collectStyles(materialUiSheets.collect(<App {...props} />))
+                })
 
-      const initialProps = await Document.getInitialProps(ctx);
+            const initialProps = await Document.getInitialProps(ctx)
 
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {styledComponentsSheet.getStyleElement()}
-            {materialUiSheets.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      styledComponentsSheet.seal();
-      materialUiSheets.getStyleElement();
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {styledComponentsSheet.getStyleElement()}
+                        {materialUiSheets.getStyleElement()}
+                    </>
+                )
+            }
+        } finally {
+            styledComponentsSheet.seal()
+            materialUiSheets.getStyleElement()
+        }
     }
-  }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
-            rel="stylesheet"
-          />
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css" rel="stylesheet" />
 
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
-          `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+          `
+                        }}
+                    />
+                    <script src={'/fullStory.js'} type="text/javascript" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
